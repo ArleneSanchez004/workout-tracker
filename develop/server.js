f7b6c501c63e2a7acec23c378819a287a7fceeb1
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-06-10 18:15:33 -0400

    added htmlRoutes.js, updated server.js to include html routes

--- develop/server.js
+++ develop/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,7 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:workout", 
+    { useNewUrlParser: true,
+    useFindAndModify: false });
+
+app.use("/", htmlRoutes);
+app.use("/exercise", htmlRoutes);
+app.use("/stats", htmlRoutes)
+//app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
     console.log("App running on port http://localhost:" + PORT);
